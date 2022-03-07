680c6f6596823e4f97c75749719bc88c79a65b12
Olivier <olivier.lesnicki@gmail.com>
2022-03-07 12:24:26 +0100

    Changed

--- pages/api/eboo/[index].js
+++ pages/api/eboo/[index].js
@@ -10,7 +10,7 @@ export default async (req, res) => {
     name: `Eboo #${index}`,
     description:
       "Eboos is a collection of 8,192 randomly generated PFP NFTs that exist on the Ethereum blockchain. This will probably be your first NFT but definitely not your last. Visit [www.eboosnft.com](https://www.eboosnft.com/) to learn more.",
-    image: "https://eboosnft.com/",
+    image: "https://eboosnft.com/default.png",
     attributes: [],
   });
 };
