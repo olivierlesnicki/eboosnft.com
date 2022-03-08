e3bca48b7bebf2f5625105fde73d6668c673300f
Olivier <olivier.lesnicki@gmail.com>
2022-03-08 10:19:01 +0100

    Changed tomorrow to later

--- src/Hero/HeroSoldOut.js
+++ src/Hero/HeroSoldOut.js
@@ -3,7 +3,7 @@ export default function HeroSoldOut() {
     <div className="flex flex-col items-center">
       <div className="text-xl sm:text-2xl max-w-2xl mb-8">
         We release 16 eboos every day. <br />
-        Come back tomorrow.
+        Come back later.
       </div>
     </div>
   );
