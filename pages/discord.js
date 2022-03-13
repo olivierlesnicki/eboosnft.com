1c9c28332fb04a2cd7aeab9a61339620c7488d12
Olivier <olivier.lesnicki@gmail.com>
2022-03-13 21:38:03 +0100

    Discord

--- pages/discord.js
+++ pages/discord.js
@@ -1,9 +1,14 @@
 import { useEffect } from "react";
 
-export default function () {
-  useEffect(() => {
-    window.location = "https://discord.gg/pBqfkSBRUE";
-  }, []);
-
+export default function Discord() {
   return null;
+}
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      permanent: true,
+      destination: "https://discord.gg/EZhcAQYF5r",
+    },
+  };
 }
